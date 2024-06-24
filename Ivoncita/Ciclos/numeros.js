f92e208e702c579b46848f92801cfe3055ec0a31
dev-rvdl <dev-rvdl@outlook.es>
2024-06-24 00:42:12 -0600

    Ciclos en js

---
+++ Ivoncita/Ciclos/numeros.js
@@ -0,0 +1,21 @@
+var numero = 1;
+
+while(numero <= 1000){
+
+    if(numero % 2 == 0 ){
+        console.log("Numero: " + numero);
+    }
+    // numero = numero +1;
+    numero++;
+}
+
+var regreso = 2000;
+
+while(regreso >= 0){
+    if (regreso % 3 == 0){
+        console.log("Regresando en " + regreso)
+    }
+    // regreso = regreso -1;
+    regreso--;
+}
+
---
+++ Ivoncita/Ciclos/numeros2.js
@@ -0,0 +1,8 @@
+//for(inicialización, condición, incremento o decremento)
+for (var i = 1; i <= 100; i++){
+    console.log("Numero: " + i);
+}
+
+for (var j = 200; j >= 0; j-- ){
+    console.log("Regeso: " + j);
+}
